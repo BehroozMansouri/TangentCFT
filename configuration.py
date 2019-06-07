ed64d05f739c9385025af756fa98604f7b9075ca
bm3302 <bm3302@rit.edu>
2019-06-07 17:10:43 -0400

    commit

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ class configuration:
     def __init__(self, config_file_path):
         file = open(config_file_path)
         self.file_path_fasttext = file.readline().rstrip('\n').split(",")[1]
-        self.result_vector_file_path = file.readline().rstrip('\n').split(",")[1]
+        self.id = int(file.readline().rstrip('\n').split(",")[1])
         self.result_file_path = file.readline().rstrip('\n').split(",")[1]
         self.vector_size = int(file.readline().rstrip('\n').split(",")[1])
         self.context_window_size = int(file.readline().rstrip('\n').split(",")[1])
