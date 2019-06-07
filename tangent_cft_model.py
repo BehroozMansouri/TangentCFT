4c1e04d2ac7b59124b046bb556b77ba4831cf081
bm3302 <bm3302@rit.edu>
2019-06-07 14:01:23 -0400

    commit

--- tangent_cft_model.py
+++ tangent_cft_model.py
@@ -2,10 +2,12 @@ from gensim.models import FastText
 import os
 import datetime
 
+
 class tangent_cft_model:
     def __init__(self, config):
         self.FastTextInput = []
         self.config = config
+        self.model = None
 
     def train(self):
         """
@@ -18,36 +20,35 @@ class tangent_cft_model:
         sg = int(self.config.skip_gram)
         hs = int(self.config.hs)
         negative = int(self.config.negative)
-        iter = int(self.config.iter)
+        iteration = int(self.config.iter)
         min_n = int(self.config.min)
         max_n = int(self.config.max)
         word_ngrams = int(self.config.ngram)
 
-        for i in range(1, 17):
-            path = self.config.filepath_fasttext + str(i)
-            for filename in os.listdir(path):
-                file = open(path + "/" + filename)
-                line = file.readline()
-                lst = []
-                while line:
-                    line = line.rstrip('\n').strip()
-                    if line is not "":
-                        lst.append(line)
+        for directory in os.listdir(self.config.filepath_fasttext):
+            "Wikipedia pages in our dataset are in folders 1 to 16, only directory for queries is named Queries"
+            if directory != "Queries":
+                path = self.config.filepath_fasttext + directory
+                for filename in os.listdir(path):
+                    file = open(path + "/" + filename)
                     line = file.readline()
-                file.close()
+                    lst = []
+                    while line:
+                        line = line.rstrip('\n').strip()
+                        if line is not "":
+                            lst.append(line)
+                        line = file.readline()
+                    file.close()
+                    self.FastTextInput.append(lst)
 
+        train_start_time = datetime.datetime.now()
 
-                self.FastTextInput.append(lst)
-                #####################################################################Rotation
-                # for idx in range(1, len(lst)):
-                #     self.FastTextInput.append(rotate(lst,1))
-                ####################################################################
-        print(datetime.datetime.now())
         self.model = FastText(self.FastTextInput, size=size, window=window, sg=sg, hs=hs,
-                              workers=1, negative=negative, iter=iter, min_n=min_n,
+                              workers=1, negative=negative, iter=iteration, min_n=min_n,
                               max_n=max_n, word_ngrams=word_ngrams)
-        print(datetime.datetime.now())
+
+        "return model train time"
+        return train_start_time - datetime.datetime.now()
 
     def get_vector(self, math_tuple):
         return self.model.wv[math_tuple]
-
