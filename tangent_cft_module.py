42470d69942a10b0c36a7125c746300c8e61ce37
bm3302 <bm3302@rit.edu>
2019-06-08 17:38:40 -0400

    commit

--- tangent_cft_module.py
+++ tangent_cft_module.py
@@ -21,9 +21,13 @@ class tangent_cft_module:
             be saved is defined in this file.
             Finally this file has the hyper_parameter setting for fasttext.
         """
+        print("Setting Configuration")
         self.configuration = configuration(config_file_path)
-        self.model = tangent_cft_model(self.configuration)
-        print("model configuration is set")
+
+        print("Reading train data")
+        self.fast_text_train_data, self.collection_formula_map = self.read_training_data()
+        self.query_formula_map = self.read_query_data()
+        self.model = tangent_cft_model(self.configuration, self.fast_text_train_data)
 
     def run(self, run_id, save_vectors):
         """
@@ -37,12 +41,12 @@ class tangent_cft_module:
 
         print("Vectors are being created.")
         self.create_result_directories(save_vectors)
-        doc_id_map, doc_tensors = self.save_Wikipedia_formulas_vectors(save_vectors)
+        doc_id_map, doc_tensors = self.get_collection_formula_vector(save_vectors)
         query_vector_map = self.save_query_formulas_vectors(save_vectors)
 
         sum = .0
         counter = 0
-        f = open("/Retrieval_Results/res_"+str(self.configuration.id), 'w')
+        f = open("/Retrieval_Results/res_" + str(self.configuration.id), 'w')
         for queryId in query_vector_map:
             query_vec = query_vector_map[queryId]
             t1 = datetime.datetime.now()
@@ -67,24 +71,7 @@ class tangent_cft_module:
         print("Average retrieval time:")
         print(sum / counter)
 
-    def read_train_data_file(self):
-        for directory in os.listdir(self.config.filepath_fasttext):
-            "Wikipedia pages in our dataset are in folders 1 to 16, only directory for queries is named Queries"
-            if directory != "Queries":
-                path = self.config.filepath_fasttext + directory
-                for filename in os.listdir(path):
-                    file = open(path + "/" + filename)
-                    line = file.readline()
-                    lst = []
-                    while line:
-                        line = line.rstrip('\n').strip()
-                        if line is not "":
-                            lst.append(line)
-                        line = file.readline()
-                    file.close()
-                    self.FastTextInput.append(lst)
-
-    def save_Wikipedia_formulas_vectors(self, save_vectors):
+    def get_collection_formula_vector(self, save_vectors):
         """
             This method for through each of the 17 Wikipedia directories, go through them file by file.
             Each file is a formula in Wikipedia dataset and for each of them we are creating a vector representation.
@@ -94,54 +81,19 @@ class tangent_cft_module:
         result = {}
         numpy_lst = []
         idx = 0
+        for formula_id in self.collection_formula_map:
+            formula_vector = self.get_vector_representation(self.collection_formula_map[formula_id])
+            numpy_lst.append(formula_vector)
+            result[idx] = formula_id
+            idx += 1
 
-        for i in range(1, 17):
-            file_path = self.configuration.file_path_fasttext + str(i)
-            for filename in os.listdir(file_path):
-                formula_vector = self.get_formula_vector(file_path + "/" + filename)
-                numpy_lst.append(formula_vector)
-                result[idx] = filename
-                idx += 1
-
-                if save_vectors:
-                    numpy.savetxt(self.configuration.result_vector_file_path + "/" + str(i) + "/" + filename,
-                                  formula_vector, newline=" ")
+            if save_vectors:
+                numpy.savetxt(self.configuration.result_vector_file_path + "/" + str(formula_id),
+                              formula_vector, newline=" ")
 
         temp = numpy.concatenate(numpy_lst, axis=0)
         tensor_values = Variable(torch.tensor(temp).double()).cuda()
         return result, tensor_values
-
-    def get_formula_vector(self, file_path):
-        """
-        This method take the converted-tuple formula file path (the file on which a list the converted tuples for
-        formula is saved, then it get vector representation of each of the tuple. The formula vector is the average of its
-        tuples vectors.
-        :param file_path: the formula file path where the fasttext tuple of that formula exists
-        :return: return the vector representation of formula based on model trained
-        """
-        sourcefile = open(file_path)
-
-        # read each slt for a formula and convert its element to char
-        line = sourcefile.readline()
-        temp_vector = None
-        first = True
-        counter = 0
-        while line:
-            line = line.rstrip('\n').strip()
-            # if the tuple vector cannot be extracted due to unseen n-gram, then we pass over that tuple.
-            try:
-                if first:
-                    temp_vector = self.model.get_vector(line)
-                    first = False
-                else:
-                    temp_vector = temp_vector + self.model.get_vector(line)
-                counter = counter + 1
-            except:
-                print(file_path)
-                pass
-            line = sourcefile.readline()
-        sourcefile.close()
-        return temp_vector / counter
 
     def save_query_formulas_vectors(self, save_vectors):
         """
@@ -149,14 +101,13 @@ class tangent_cft_module:
         We have 40 queries in the dataset of which the second 20 are wildcard queries.
         """
         query_vectors = {}
-        file_path_query = self.configuration.file_path_fasttext + "Queries/"
-        for i in range(1, number_of_queries+1):
-            query_vector = self.get_formula_vector(file_path_query + str(i) + ".txt")
+        for i in range(1, number_of_queries + 1):
+            query_vector = self.get_vector_representation(i)
             query_vectors[i] = Variable(torch.tensor(query_vector).double()).cuda()
 
             if save_vectors:
                 numpy.savetxt(self.configuration.result_vector_file_path + "/Queries/" + str(i) + ".txt", query_vector,
-                          newline=" ")
+                              newline=" ")
         return query_vectors
 
     def create_result_directories(self, save_vectors):
@@ -170,3 +121,12 @@ class tangent_cft_module:
         for i in range(1, 17):
             os.makedirs(self.configuration.result_vector_file_path + "/" + str(i))
         os.makedirs(self.configuration.result_vector_file_path + "/" + "Queries")
+
+    def read_training_data(self):
+        pass
+
+    def read_query_data(self):
+        pass
+
+    def get_vector_representation(self, formula_fast_text_representation):
+        pass
