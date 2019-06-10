e0d7e6216a215123993cdf89968acc110363a040
bm3302 <bm3302@rit.edu>
2019-06-10 16:54:02 -0400

    commit

--- encoder_formula_level.py
+++ encoder_formula_level.py
@@ -46,7 +46,7 @@ def tangent_to_fasttext(tangent_tuple_filepath, result_file_path, ignore_frp=Tru
 
 def main():
     source = '/home/bm3302/FastText/SLTTuples_W1/'
-    destination = '/home/bm3302/FastText/s1_map'
+    destination = '/home/bm3302/FastText/s1_map/'
     tangent_to_fasttext(source, destination, ignore_frp=True)
 
 
