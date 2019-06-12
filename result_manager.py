c758ec578a95d53b1079c194bf3b4210b49a4ffe
bm3302 <bm3302@rit.edu>
2019-06-12 11:05:02 -0400

    commit

--- result_manager.py
+++ result_manager.py
@@ -1,15 +1,15 @@
 import subprocess
 import os.path
 from sys import argv
-import configuration
+from configuration import configuration
 
 
-def main(argv):
+def main():
 
-    id_1 = int(argv[1])
-    id_2 = int(argv[2])
+    id_1 = 7059#int(argv[1])
+    id_2 = 7060#int(argv[2])
     "file path to save the result of the analysis"
-    result_file_path = argv[3]
+    result_file_path = "result5.csv"#argv[3]
     results_by_parameters(id_1, id_2, result_file_path)
 
 
@@ -17,14 +17,14 @@ def results_by_parameters(start_file_id, end_file_id, result_file_path):
     result_file = open(result_file_path, "w")
     process_name = "trec_eval"
     judge_file = "judge.dat"
-    config = configuration("/config/config_" + str(start_file_id) + ".csv")
+    config = configuration("config/config_" + str(start_file_id))
     dic = config.__dict__
     result_file.write("file_id,")
     for val in dic:
         result_file.write(str(val) + ",")
     result_file.write("partial_bpref, full_bpref\n")
     for file_id in range(start_file_id, end_file_id + 1):
-        result_file_name = "/Retrieval_Results/res_" + str(file_id)
+        result_file_name = "Retrieval_Results/res_" + str(file_id)
         if not os.path.isfile(result_file_name):
             continue
         temp = subprocess.check_output(
@@ -34,7 +34,7 @@ def results_by_parameters(start_file_id, end_file_id, result_file_path):
         temp = subprocess.check_output(
             [process_name, judge_file, result_file_name, "-l3"])
         full_bpref = temp.decode("utf-8").split("\n")[8].split("\t")[2]
-        config = configuration("/config/config_" + str(file_id) + ".txt")
+        config = configuration("config/config_" + str(file_id))
         dic = config.__dict__
         result_file.write(str(file_id) + ",")
         for val in dic.values():
@@ -44,4 +44,4 @@ def results_by_parameters(start_file_id, end_file_id, result_file_path):
 
 
 if __name__ == "__main__":
-    main(argv)
+    main()
