73be5457721b4a8e8cb2d8be588db695c2ca34dc
BehroozMansouri <b.m.mansouri@gmail.com>
2019-11-04 10:09:40 -0500

    Add files via upload

--- Visualization/formula_visualization.py
+++ Visualization/formula_visualization.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 
-# Create data
-def read_file_address(file_path):
-    file = open(file_path)
+def read_data_points(file_path_formula_categories, file_path_2d_vectors_of_formulas):
+    file = open(file_path_formula_categories)
     line = file.readline().strip("\n")
     data_points = {}
     while line:
         sections = line.split(",")
-        file2 = open("/home/bm3302/FastText/Run_Result_9012/" + sections[1] + ".txt")
+        file2 = open(file_path_2d_vectors_of_formulas + sections[1] + ".txt")
         vector = np.fromfile(file2, sep=" ")
         if sections[0] in data_points.keys():
             data_points[sections[0]].append(vector)
@@ -21,25 +20,14 @@ def read_file_address(file_path):
     return data_points
 
 
-def read_label(file_path):
-    lst = []
-    file = open(file_path)
-    line = file.readline().strip("\n")
-    while line:
-        sections = line.split(",")
-        lst.append(sections[1])
-        line = file.readline().strip("\n")
-    return lst
-
-
-def draw_map(file_path):
-    map = read_file_address(file_path)
+def draw_map(file_path_formula_categories, file_path_2d_vectors_of_formulas):
+    vector_2d_formulas = read_data_points(file_path_formula_categories, file_path_2d_vectors_of_formulas)
     x = []
     y = []
-
-    lst_colors = ["red", "blue", "green", "purple", "black", "silver", "cyan", "gold", "violet"]
+    colors = ["red", "blue", "green", "purple", "black", "silver", "cyan", "gold", "violet"]
     markers = ['o', '*', 'X', '^', 'D', 'h', 'd', 'v', 's']
-    labels = ["Matrix", "Integral", "Series", "Limit", "Logarithm", "Trigonometric", "Set Theory", "Probability", "Derivative"]
+    labels = ["Matrix", "Integral", "Series", "Limit", "Logarithm", "Trigonometric", "Set Theory", "Probability",
+              "Derivative"]
     color = []
     marker = []
     max_x = -100
@@ -47,8 +35,8 @@ def draw_map(file_path):
     max_y = -100
     min_y = 100
 
-    for key in map.keys():
-        for item in map[key]:
+    for key in vector_2d_formulas.keys():
+        for item in vector_2d_formulas[key]:
             x.append(item[0])
             if item[0] > max_x:
                 max_x = item[0]
@@ -59,21 +47,20 @@ def draw_map(file_path):
                 max_y = item[1]
             elif item[0] < min_y:
                 min_y = item[0]
-            color.append(lst_colors[int(key) - 1])
+            color.append(colors[int(key) - 1])
             marker.append(markers[(int(key))-1])
 
     x_range = max_x - min_x
     y_range = max_y - min_y
-
     x = (2 * (x - min_x) / x_range) - 1
     y = (2 * (y - min_y) / y_range) - 1
     legend_info = []
-    for counter in range(0, len(lst_colors)):
-        legend_info.append(Line2D([0], [0], color='w', label=labels[counter], markerfacecolor=lst_colors[counter],
+    for counter in range(0, len(colors)):
+        legend_info.append(Line2D([0], [0], color='w', label=labels[counter], markerfacecolor=colors[counter],
                                   marker=markers[counter], markersize=8))
 
     fig, ax = plt.subplots()
-    for key in map.keys():
+    for key in vector_2d_formulas.keys():
         key = int(key)
         start = (key-1) * 20
         end = start + 20
@@ -84,14 +71,9 @@ def draw_map(file_path):
 
 
 def main():
-    """
-    A sample drawing tool, takes the directory of formulas on the file "formula_tsne".
-    and read the vectors from the directory where vector representation of formulas (on which T-SNE is applied)
-    are located.
-
-    :return:
-    """
-    draw_map("formula_tsne")
+    file_path_formula_categories = "formula_tsne"
+    file_path_2d_vectors_of_formulas = "applied_tsne_file"
+    draw_map(file_path_formula_categories, file_path_2d_vectors_of_formulas)
 
 
 if __name__ == '__main__':
