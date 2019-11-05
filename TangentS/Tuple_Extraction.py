66116ff00481a26c6585f9b99ddbd34f680c1f76
BehroozMansouri <b.m.mansouri@gmail.com>
2019-11-04 20:30:08 -0500

    Add files via upload

--- TangentS/Tuple_Extraction.py
+++ TangentS/Tuple_Extraction.py
@@ -11,5 +11,3 @@ def latex_math_to_opt_tuples(latex_formula):
     temp = MathExtractor.parse_from_tex_opt(latex_formula)
     aaa = temp.get_pairs(window=2, eob=True)
     return aaa
-
-print(latex_math_to_opt_tuples("so $2q^2$.  has an odd number of factors of 2. My salary was $500 but it was good. This formula $\$=1$ is hard."))
