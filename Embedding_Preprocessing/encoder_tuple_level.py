643c92cd7ce254a6e7396ab8d04b16eb96a0502b
BehroozMansouri <b.m.mansouri@gmail.com>
2021-09-07 17:36:53 -0400

    Update encoder_tuple_level.py

--- Embedding_Preprocessing/encoder_tuple_level.py
+++ Embedding_Preprocessing/encoder_tuple_level.py
@@ -1,6 +1,4 @@
-import os
 from enum import Enum
-import os.path
 
 
 class TupleTokenizationMode(Enum):
@@ -20,41 +18,15 @@ class TupleTokenizationMode(Enum):
 
 
 class TupleEncoder:
-    def __init__(self,):
-        self.__encoder_map_node = {}
-        self.__encoder_map_path = {}
-        self.__create_path_map()
-        self.update_list = {}
-        self.node_id = None
-
-    def __create_path_map(self):
-        # Symbol_Layout_Tree edge labels
-        self.__encoder_map_path["n"] = chr(500)
-        self.__encoder_map_path["w"] = chr(501)
-        self.__encoder_map_path["a"] = chr(502)
-        self.__encoder_map_path["b"] = chr(503)
-        self.__encoder_map_path["e"] = chr(504)
-        self.__encoder_map_path["u"] = chr(505)
-        self.__encoder_map_path["o"] = chr(506)
-        self.__encoder_map_path["c"] = chr(507)
-        self.__encoder_map_path["d"] = chr(508)
-        # Operator_Tree edge labels
-        self.__encoder_map_path["0"] = chr(509)
-        self.__encoder_map_path["1"] = chr(510)
-        self.__encoder_map_path["2"] = chr(511)
-        self.__encoder_map_path["3"] = chr(512)
-        self.__encoder_map_path["4"] = chr(513)
-        self.__encoder_map_path["5"] = chr(514)
-        self.__encoder_map_path["6"] = chr(515)
-        self.__encoder_map_path["7"] = chr(516)
-        self.__encoder_map_path["8"] = chr(517)
-        self.__encoder_map_path["9"] = chr(518)
-
-    def encode_tuples(self, node_map, node_id, math_tuples, embedding_type=TupleTokenizationMode.Both_Separated,
+    @staticmethod
+    def encode_tuples(node_map, edge_map, node_id, edge_id, math_tuples,
+                      embedding_type=TupleTokenizationMode.Both_Separated,
                       ignore_full_relative_path=True, tokenize_all=False, tokenize_number=True):
         """
         Takes the encoder map (which can be empty) and the last node id and enumerates the tuple tokens to converts the
         tuples to words (with n-gram as each tokenized tuple element) to make the formulas ready to be fed to fasttext
+        :param edge_map: dictionary of tokens related to the path with between nodes in trees
+        :param edge_id: the id of last edge token
         :param node_map: dictionary of tokens and their id
         :param node_id: the last node id
         :param math_tuples: list of formula tuples (which are extracted by Tangent-S) to be encoded
@@ -62,46 +34,60 @@ class TupleEncoder:
         :param ignore_full_relative_path: determines to ignore the full relative path or not (default True)
         :param tokenize_all: determines to tokenize all elements (such as numbers and text) (default False)
         :param tokenize_number: determines to tokenize the numbers or not (default True)
-        :return:
+        :return: list of encoded tuples
         """
-        self.node_id = node_id
-        self.__encoder_map_node = node_map
         encoded_tuples = []
+        update_map_node = {}
+        update_map_edge = {}
         for math_tuple in math_tuples:
-            encoded_slt_tuple = ""
-            slt_elements = self.__get_slt_elements(math_tuple, ignore_full_relative_path=ignore_full_relative_path)
+            encoded_tuple = ""
+            tuple_elements = TupleEncoder.__get_tuple_elements(math_tuple,
+                                                               ignore_full_relative_path=ignore_full_relative_path)
 
-            converted_value = self.__convert_node_elements(slt_elements[0], embedding_type,
-                                                           tokenize_all=tokenize_all,
-                                                           tokenize_number=tokenize_number)
-            encoded_slt_tuple = encoded_slt_tuple + converted_value
+            # Encoding Element 1
+            converted_value, node_map, node_id, update_map_node = TupleEncoder.__convert_node_elements(
+                tuple_elements[0], node_map, node_id, update_map_node, embedding_type, tokenize_all=tokenize_all,
+                tokenize_number=tokenize_number)
+            encoded_tuple = encoded_tuple + converted_value
 
-            converted_value = self.__convert_node_elements(slt_elements[1], embedding_type,
-                                                           tokenize_all=tokenize_all,
-                                                           tokenize_number=tokenize_number)
-            encoded_slt_tuple = encoded_slt_tuple + converted_value
+            # Encoding Element 2
+            converted_value, node_map, node_id, update_map_node = TupleEncoder.__convert_node_elements(
+                tuple_elements[1], node_map, node_id, update_map_node, embedding_type, tokenize_all=tokenize_all,
+                tokenize_number=tokenize_number)
+            encoded_tuple = encoded_tuple + converted_value
 
-            converted_value = self.__convert_path_elements(slt_elements[2])
-            encoded_slt_tuple = encoded_slt_tuple + converted_value
+            # Encoding Edge
+            converted_value, edge_map, edge_id, update_map_edge = TupleEncoder.__convert_path_elements(
+                tuple_elements[2], edge_map, edge_id, update_map_edge)
+            encoded_tuple = encoded_tuple + converted_value
+
+
             "Encode the full relative path"
             if not ignore_full_relative_path:
-                converted_value = self.__convert_path_elements(slt_elements[3])
-                encoded_slt_tuple = encoded_slt_tuple + converted_value
-            encoded_tuples.append(encoded_slt_tuple)
+                converted_value, edge_map, edge_id, update_map_edge = TupleEncoder.__convert_path_elements(
+                    tuple_elements[3], edge_map, edge_id, update_map_edge)
+                encoded_tuple = encoded_tuple + converted_value
+            encoded_tuples.append(encoded_tuple)
 
-        temp_update_list = self.update_list
-        self.update_list = {}
-        return encoded_tuples, temp_update_list, self.node_id
+        return encoded_tuples, update_map_node, update_map_edge, node_id, edge_id
 
-    def __convert_node_elements(self, node, embedding_type, tokenize_all=False, tokenize_number=False):
+    @staticmethod
+    def __convert_node_elements(node, node_map, node_id, update_map_node, embedding_type, tokenize_all=False,
+                                tokenize_number=False):
         lst = []
         if "!" in node:
+            # This shows the tuple is a leaf in the tree and this node has only Type and no Value
             if node == "O!":
                 lst.append(node)
+            # Tuple has both Value and Type
             else:
-                node_type = node.split("!")[0]+"!"
+                # All the node Type are kept with their ! sign.
+                node_type = node.split("!")[0] + "!"
+                # Value of the node
                 node_value = node.split("!")[1]
 
+                # Check if it needs to break down numbers or other Types such as text
+                # For instance, convert "sin" to "s" "i" "n"
                 if embedding_type == TupleTokenizationMode.Type:
                     lst.append(node_type)
                 elif embedding_type == TupleTokenizationMode.Value:
@@ -121,30 +107,38 @@ class TupleEncoder:
                     lst.append(node)
         else:
             lst.append(node)
-        return self.__get_char_value(lst)
-
-    def __convert_path_elements(self, path):
-        converted_value = ""
-        for label in path:
-            value = self.__encoder_map_path.get(label)
-            converted_value += value
-        return converted_value
-
-    def __get_char_value(self, lst):
-        converted_value = ""
-        for item in lst:
-            if item in self.__encoder_map_node:
-                value = chr(self.__encoder_map_node.get(item))
-            else:
-                value = chr(self.node_id)
-                self.update_list[item] = self.node_id
-                self.__encoder_map_node[item] = self.node_id
-                self.node_id = self.node_id + 1
-            converted_value += value
-        return converted_value
+        return TupleEncoder.__get_char_value(lst, node_map, node_id, update_map_node)
 
     @staticmethod
-    def __get_slt_elements(tangent_tuple, ignore_full_relative_path=True):
+    def __convert_path_elements(path, edge_map, edge_id, update_map_edge):
+        converted_value = ""
+        for label in path:
+            if label in edge_map:
+                value = chr(edge_map[label])
+            else:
+                value = chr(edge_id)
+                update_map_edge[label] = edge_id
+                edge_map[label] = edge_id
+                edge_id += 1
+            converted_value += value
+        return converted_value, edge_map, edge_id, update_map_edge
+
+    @staticmethod
+    def __get_char_value(lst, map_node, last_id, update_map):
+        converted_value = ""
+        for item in lst:
+            if item in map_node:
+                value = chr(map_node.get(item))
+            else:
+                value = chr(last_id)
+                update_map[item] = last_id
+                map_node[item] = last_id
+                last_id += 1
+            converted_value += value
+        return converted_value, map_node, last_id, update_map
+
+    @staticmethod
+    def __get_tuple_elements(tangent_tuple, ignore_full_relative_path=True):
         if ignore_full_relative_path:
             return tangent_tuple.split('\t')[:3]
         return tangent_tuple.split('\t')[:4]
